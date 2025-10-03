31dc501739cd04dd0f38a5077310f95917c503c9
Yash Krishna Pille <yashpille2007@gmail.com>
2025-10-03 16:51:10 +0530

    Add files via upload

---
+++ assignment 2 task 2.py
@@ -0,0 +1,11 @@
+# Program to calculate the sum of numbers from 1 to 50
+
+# Step 1: Initialize sum
+total = 0
+
+# Step 2: Use for loop to add numbers from 1 to 50
+for i in range(1, 51):
+    total += i
+
+# Step 3: Display the result
+print("The sum of numbers from 1 to 50 is:" ,total)
